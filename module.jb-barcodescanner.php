c99e85966b8584e6bbe05b99d1456e476d8ab5db
jbostoen <->
2021-03-17 13:06:47 +0100

    Initial work

---
+++ module.jb-barcodescanner.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021 Jeffrey Bostoen
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2021-03-17 12:23:03
+ *
+ * iTop module definition file
+ */
+
+SetupWebPage::AddModule(
+        __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+        'jb-barcodescanner/2.6.210317',
+        array(
+                // Identification
+                //
+                'label' => 'Datamodel: Barcode Scanner',
+                'category' => 'business',
+
+                // Setup
+                //
+                'dependencies' => array( 
+					'itop-config-mgmt/2.6.0'
+                ),
+                'mandatory' => false,
+                'visible' => true,
+
+                // Components
+                //
+                'datamodel' => array(
+					'model.jb-barcodescanner.php'
+                ),
+                'webservice' => array(
+
+                ),
+                'data.struct' => array(
+					// add your 'structure' definition XML files here,
+                ),
+                'data.sample' => array(
+					// add your sample data XML files here,
+                ),
+
+                // Documentation
+                //
+                'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+                'doc.more_information' => '', // hyperlink to more information, if any
+
+                // Default settings
+                //
+                'settings' => array(
+                        // Module specific settings go here, if any
+                ),
+        )
+);
+
+
---
+++ readme.md
@@ -0,0 +1,20 @@
+# jb-barcodescanner
+Copyright (C) 2021 Jeffrey Bostoen
+
+[![License](https://img.shields.io/github/license/jbostoen/iTop-custom-extensions)](https://github.com/jbostoen/iTop-custom-extensions/blob/master/license.md)
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/jbostoen)
+🍻 ☕
+
+
+Need assistance with iTop or one of its extensions?  
+Need custom development?  
+Please get in touch to discuss the terms: **jbostoen.itop@outlook.com**
+
+## What?
+Adds a Barcode Scanner class, which is a subclass of a Physical Device
+
+## Cookbook
+
+XML:
+* add a new class
+
